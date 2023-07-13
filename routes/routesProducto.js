24f820ebd40f5a0f33ffb861c54cd27bff56eaa1
CarlosMario123 <221220@ids.upchiapas.edu.mx>
2023-07-13 08:37:09 -0600

    cambias realizado

---
+++ routes/routesProducto.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const routesProducto = express.Router();
+
+routesProducto.get("/", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
+
+    conn.query("SELECT * FROM Producto", (err, rows) => {
+      if (err) return res.send(err);
+
+      res.json(rows);
+    });
+  });
+});
+
+routesProducto.get("/:nombre", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
+    conn.query(
+      "SELECT Nombre,Cantidad_Disponible FROM Producto WHERE Nombre = ?",
+      [req.params.nombre],
+      (err, rows) => {
+        if (err) return res.send(err);
+        res.json(rows);
+      }
+    );
+  });
+});
+
+routesProducto.post("/", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
+    conn.query("INSERT INTO Producto set ?", [req.body], (err, rows) => {
+      if (err) return res.send(err);
+      res.json(rows);
+    });
+  });
+});
+
+routesProducto.delete("/:id", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
+    conn.query(
+      "DELETE FROM Producto WHERE id_Producto = ?",
+      [req.params.id],
+      (err, rows) => {
+        if (err) return res.send(err);
+
+        res.json(rows);
+      }
+    );
+  });
+});
+
+routesProducto.put("/:id", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
+    conn.query(
+      "UPDATE Producto set ? WHERE id_Producto = ?",
+      [req.body, req.params.id],
+      (err, rows) => {
+        if (err) return res.send(err);
+      }
+    );
+  });
+});
+
+module.exports = routesProducto;
